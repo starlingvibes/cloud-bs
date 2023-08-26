0088d59d1d79a15450780096c7269be9d0308f99
starlingvibes <starlingroot@protonmail.com>
2023-08-26 17:58:26 +0100

    fix(): set DB_HOST as an env

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
