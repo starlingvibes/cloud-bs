54e5b8e17725fe7346ff79fefd6b5019fbabb685
starlingvibes <starlingroot@protonmail.com>
2023-08-26 17:25:18 +0100

    fix(): enabled CORS

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express, { Express, Request, Response } from 'express';
 import authRouter from './routes/auth.route';
 import storageRouter from './routes/storage.route';
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/storage', storageRouter);
 
